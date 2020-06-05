766365d34cf7ee52a253a492e54e56a79de02b2c
brokkoli71 <brokkoli71b@gmail.com>
2020-06-06 01:48:06 +0200

    add datatype to database

--- receiver.php
+++ receiver.php
@@ -13,10 +13,14 @@
             if(isset($_POST['receiver'])){
                 $receiver = $_POST['receiver'];
             }
+            $datatype = "0";
+            if(isset($_POST['data_type'])){
+                $datatype = $_POST['data_type'];
+            }
                 
             move_uploaded_file($_FILES["data"]["tmp_name"], "files/".$filename);
             
-            $req = "INSERT INTO send_app(data_path, receiver) VALUES ('".$filename."','".$receiver."');";
+            $req = "INSERT INTO send_app(data_path, data_type, receiver) VALUES ('".$filename."','".$datatype."','".$receiver."');";
             
             $erg = $db->query($req);
             
--- sender.php
+++ sender.php
@@ -7,10 +7,10 @@
             if(isset($_POST['receiver'])){
                 $receiver = $_POST['receiver'];
                 
-                $req  = "SELECT data_path FROM send_app WHERE \"".$receiver."\"=receiver LIMIT 1";
+                $req  = "SELECT data_path, data_type FROM send_app WHERE \"".$receiver."\"=receiver LIMIT 1";
                 $erg = $db->query($req);
                 $row = $erg->fetch_assoc();
-                echo $row["data_path"];
+                echo $row["data_path"]."?".$row["data_type"];
            }
         }else{
             echo "wrong password";
