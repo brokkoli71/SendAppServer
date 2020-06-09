8fecdbcff45055bf6865b98a4b265eb3085cdb6e
brokkoli71 <brokkoli71b@gmail.com>
2020-06-09 15:55:09 +0200

    add drop_data script

--- drop_data.php
+++ drop_data.php
@@ -4,8 +4,26 @@
 
     if(isset($_POST['password'])){
         if($password == $_POST['password']){
-            if(isset($_POST['file_name'])){
-                $filename = $_POST['file_name'];
+            if(isset($_POST['task_id'])){
+                $task_id = $_POST['task_id'];
+                $error = false;
+
+                $req  = "SELECT data_path FROM send_app WHERE send_app.id = \"".$task_id."\";";
+                $erg = $db->query($req);
+
+                if ($erg->num_rows>0){
+                    $row = $erg->fetch_assoc();
+                    $filename = $row["data_path"];
+                    if(!unlink("files/".$filename)){
+                        $error = true;
+                    }
+                }
+
+                $req  = "UPDATE send_app SET received = "1" WHERE send_app.id = \"".$task_id."\";";
+                //use that later for not storing data
+                //$req  = "DELETE FROM send_app WHERE send_app.id = \"".$task_id."\";";
+                $erg = $db->query($req);
+                
             }
         }else{
             echo "wrong password";
--- sender.php
+++ sender.php
@@ -7,13 +7,15 @@
             if(isset($_POST['receiver'])){
                 $receiver = $_POST['receiver'];
                 
-                $req  = "SELECT data_path, data_type FROM send_app WHERE \"".$receiver."\"=receiver LIMIT 1";
+                $req  = "SELECT id, data_path, data_type FROM send_app WHERE \"".$receiver."\"=receiver LIMIT 1";
                 $erg = $db->query($req);
                 if ($erg->num_rows>0){
                     $row = $erg->fetch_assoc();
                     echo $row["data_path"];
                     echo "?";
                     echo $row["data_type"];
+                    echo "?";
+                    echo $row["id"];
                 }else{
                     echo "no result";
                 }
