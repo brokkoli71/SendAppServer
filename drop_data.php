def7c5d26b2faa5b1d2c77c3b6df259fffb3f5f3
brokkoli71 <brokkoli71b@gmail.com>
2020-06-09 14:43:26 +0200

    alpha 0.1.3

--- drop_data.php
+++ drop_data.php
@@ -1,7 +1,7 @@
 <?php
     include 'config.php';
     require 'db.php';
-    
+
     if(isset($_POST['password'])){
         if($password == $_POST['password']){
             if(isset($_POST['file_name'])){
